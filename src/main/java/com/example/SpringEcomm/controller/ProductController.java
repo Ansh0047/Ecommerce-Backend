a15e02bc430112c189b9dfc6a9404e0324ea8b4d
Ansh <anshkant090@gmail.com>
2025-12-12 01:04:12 +0530

    product image save and fetch done

--- src/main/java/com/example/SpringEcomm/controller/ProductController.java
+++ src/main/java/com/example/SpringEcomm/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -32,11 +34,35 @@ public class ProductController {
     public ResponseEntity<Product> getProducts (@PathVariable  int id) {
         Product product = productService.getProductById(id);
 
-        if(product.getId() > 0){
+        if(product.getId() != -1){
             return new ResponseEntity<>(product,HttpStatus.OK);
         }
         else{
             return new ResponseEntity<>(new Product(-1),HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping ("/product/{id}/image")
+    public ResponseEntity<byte[]> getImageByProductId (@PathVariable int id){
+        Product product = productService.getProductById(id);
+
+        if(product.getId() != -1){
+            return new ResponseEntity<>(product.getImageData(),HttpStatus.OK);        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping ("/product")
+    public ResponseEntity<?> addProduct (@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product savedProduct = null;
+
+        try {
+            savedProduct = productService.addProduct(product,imageFile);
+            return new ResponseEntity<>(savedProduct,HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        // return "product saved successfully...";
     }
 }
--- src/main/java/com/example/SpringEcomm/model/Product.java
+++ src/main/java/com/example/SpringEcomm/model/Product.java
@@ -4,6 +4,7 @@ package com.example.SpringEcomm.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,6 +32,11 @@ public class Product {
     private int stockQuantity;
     private boolean productAvailable;
 
+    // storing the image of the product
+    private String imageName;
+    private String imageType;
+    @Lob     // large object datatype to store massive data
+    private byte[] imageData;   // storing the image in the form of byte stream
 
     public Product(int id) {
         this.id = id;
